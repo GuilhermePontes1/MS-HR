343cce2d8e29d21ad78bcf2b44e178eed67a8e4c
GuilhermePontes01 <guilhermepontesmtm@outlook.com>
2021-12-06 12:18:24 -0300

    Project hr-worker created

---
+++ hr-workers/src/main/java/com/worker/hrworkers/HrWorkersApplication.java
@@ -0,0 +1,13 @@
+package com.worker.hrworkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrWorkersApplication.class, args);
+    }
+
+}
---
+++ hr-workers/src/test/java/com/worker/hrworkers/HrWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worker.hrworkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
