8f7ada581d9ad6aa2ae23a5aba28983868603277
GuilhermePontes01 <guilhermepontesmtm@outlook.com>
2021-12-09 09:03:34 -0300

    Add RestTemplate

---
+++ hr-payroll/src/main/java/com/workers/hrpayroll/entities/Worker.java
@@ -0,0 +1,62 @@
+package com.workers.hrpayroll.entities;
+
+
+import java.io.Serializable;
+
+
+public class Worker implements Serializable {
+private static final Long serialVersionUID = 1L;
+
+private Long id;
+private String name;
+private Double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Worker)) return false;
+
+        Worker worker = (Worker) o;
+
+        return getId() != null ? getId().equals(worker.getId()) : worker.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
+
--- hr-payroll/src/main/java/com/workers/hrpayroll/services/PayamentService.java
+++ hr-payroll/src/main/java/com/workers/hrpayroll/services/PayamentService.java
@@ -1,13 +1,31 @@
 package com.workers.hrpayroll.services;
 
 import com.workers.hrpayroll.entities.Payament;
+import com.workers.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PayamentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payament getPayament(Long workerId, int days) {
-        return new Payament("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        return new Payament(worker.getName(),worker.getDailyIncome(), days);
     }
 
 }
