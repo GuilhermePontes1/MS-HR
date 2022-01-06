227b721bedb80d97fd124f14564540fb7a839a29
GuilhermePontes01 <guilhermepontesmtm@outlook.com>
2022-01-06 16:48:02 -0300

    hr-worker as a config server client

--- hr-eureka-server/src/main/java/com/worker/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/worker/hreurekaserver/HrEurekaServerApplication.java
@@ -3,6 +3,7 @@ package com.worker.hreurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
--- hr-workers/src/main/java/com/worker/hrworkers/controller/WorkerController.java
+++ hr-workers/src/main/java/com/worker/hrworkers/controller/WorkerController.java
@@ -5,6 +5,7 @@ import com.worker.hrworkers.repository.WorkRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,20 @@ public class WorkerController {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private WorkRepository repository;
 
     @Autowired
     private Environment env;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -40,6 +50,7 @@ public class WorkerController {
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
+
 
 }
 
